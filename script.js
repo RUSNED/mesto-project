f3f28200dbea8139394d1bcd3f82312049e01b45
Ruslan Nedbailo <ya.slik3109@yandex.ru>
2022-09-16 13:32:05 +0300

    js2

--- script.js
+++ script.js
@@ -35,3 +35,59 @@ like.forEach(function(likeIcon) {
   });
 });
 
+
+// Находим форму в DOM
+const formElement = document.querySelector('#popup')
+// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = document.querySelector('#profile__name');// Воспользуйтесь инструментом .querySelector()
+const jobInput = document.querySelector('#profile-description');// Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    nameInput.getAttribute('value');
+    jobInput.getAttribute('value');
+    // Выберите элементы, куда должны быть вставлены значения полей
+    const profileName = document.querySelector('#profileName');
+    const profileJob = document.querySelector('#profileJob');
+    // Вставьте новые значения с помощью textContent
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
+
+const saveButton = document.querySelector('#saveButton');
+saveButton.addEventListener('click', function() {
+  closePopup(popup);
+})
+
+
+const cardContainer = document.querySelector('#cardContainer');
+
+function cardActive () {
+    const elementTemplate = document.querySelector('#elementTemplate').content;
+    const cardElement = elementTemplate.querySelector('.element__card').cloneNode(true);
+
+    cardElement.querySelector('.element__image').src = "./images/Karachaevsk.png";
+    cardElement.querySelector('.element__place').textContent = 'Дюк Корморант';
+
+    cardContainer.prepend(cardElement); 
+}
+
+
+cardButton.addEventListener('click',function () {
+    cardActive();
+    
+});
+
+
+
